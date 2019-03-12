49196a27fbb0b9b32b00b463b25340bd82d8d2c8
sairuby <vnsaitheja@gmail.com>
2019-03-11 21:53:09 -0400

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,26 @@
+require 'bundler/setup'
+require 'rubygems'
+require 'sinatra'
+require 'json'
+require 'logger'
+require 'yaml'
+require 'fileutils'
+require 'pry'
+require 'uri'
+require 'net/http'
+
+ 
+post '/testcall' do
+
+  #a = request.params['text']
+  #b = request.params['trigger_word']
+  #h = a.slice! b
+  result  = "hello #{params}"
+  content_type :json
+  {:text => "#{result1}"}.to_json
+end
+get '/hello' do
+	" hello"
+end
+
+#https://hooks.slack.com/services/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
